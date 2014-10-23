4515e0975d36b0bb03a243f5bfe3020b1e04ae05
Ben Visser <theodore.r.visser@gmail.com>
2014-10-23 11:37:15 -0700

    Media player queue complete

--- client/lib/file-io.js
+++ client/lib/file-io.js
@@ -407,26 +407,50 @@ function cancel_file(id) {
 
 /* Play media */
 function play_file(id, title, type) {
-     var url = $('#' + id).children('a').first().attr('href');
+  var url = $('#' + id).children('a').first().attr('href');
 
-     // if ($('source').length > 0) {
-     //      $('<source/>',{'type': type,'src': url, 'title': title}).appendTo('audio');
-     // } else {
-          // $('source').remove();
+  var audio;
+  var playlist;
+  var tracks;
+  var current;
 
-          $('<li><a href=' + url + '>' + title + '</a></li>').appendTo('#playlist');
-          initPlaylist();
-          // $('#music-player').removeAttr('style');
-          // $('audio').mediaelementplayer({success:function(media){
-  //            media.play();
-  //      }});
+  $('<li><a href=' + url + '>' + title + '</a></li>').appendTo('#playlist');
 
-// <audio controls="controls">
-//           <source src="shit" title="shit" type="audio/mp3"/>
-//         </audio>
-       // num += 1;
-     // }
-
+  initPlaylist();
+  function initPlaylist(){
+    current = 0;
+    audio = $('audio');
+    playlist = $('#playlist');
+    tracks = playlist.find('li a');
+    console.log(tracks);
+    len = tracks.length - 1;
+    audio[0].volume = .70;
+    audio[0].play();
+    playlist.find('a').click(function(e){
+        e.preventDefault();
+        link = $(this);
+        current = link.parent().index();
+        run(link, audio[0]);
+    });
+    audio[0].addEventListener('ended',function(e){
+        current++;
+        if(current == len){
+            current = 0;
+            link = playlist.find('a')[0];
+        }else{
+            link = playlist.find('a')[current];    
+        }
+        run($(link),audio[0]);
+    });
+  }
+  
+  function run(link, player){
+          player.src = link.attr('href');
+          par = link.parent();
+          par.addClass('active').siblings().removeClass('active');
+          audio[0].load();
+          audio[0].play();
+  }
 }
 
 /* creates an entry in our filelist for a user, if it doesn't exist already - TODO: move this to script.js? */
