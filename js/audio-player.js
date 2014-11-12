621711eb94248c34f2b79356affe87274d694e44
devDesign <marlyguha@gmail.com>
2014-11-11 19:52:51 -0800

    added indexdb mp3 blobs stay on disk and autoload

--- js/audio-player.js
+++ js/audio-player.js
@@ -131,3 +131,72 @@ $('document').ready(function(){
     duration = audio.duration;  
   }, false);
 });
+
+function play_torrent_file(url, title, type, blob) {
+  blob['type']=type
+  var audio;
+  var playlist;
+  var tracks;
+  var current;
+  var pButton = document.getElementById('pButton');
+  reader = new FileReader();
+  console.log(blob);
+  
+  reader.onload = function(event) {
+    ID3.loadTags(url, function() {
+      var tags = ID3.getAllTags(url);
+     $('<tr class="file-entry"><td><a href='+url+'>'+tags.artist+'</a></td><td><a href='+url+'>'+tags.title+'</a></td><td><a href='+url+'>'+tags.album+'</a></td></tr>').appendTo('#playlist');
+
+      initPlaylist(tags);
+      function initPlaylist(tag){
+        current = 0;
+        audio = $('#audio')[0];
+        playlist = $('#playlist');
+        tracks = playlist.find('tr');
+        len = tracks.length - 2;
+        audio.volume = .70;
+        audio.play();
+       playlist.find('a').click(function(e){
+            e.preventDefault();
+            link = $(this);
+            current = link.parent().parent().index();
+            run(link, audio);
+        });
+        audio.addEventListener('ended',function(e){
+            current++;
+            if(current == len){
+                current = 0;
+                link = playlist.find('a')[0];
+            }else{
+                var row = playlist.find('tr')[current]
+                link = $(row).find('td').find('a')[1];    
+            }
+            run($(link),audio);
+        });
+      }
+      
+      function run(link, player){
+        if (pButton.className == "play"){
+          pButton.className = "";
+          pButton.className = "pause";
+        };
+
+        var songArtist = $(link[0]).parent().parent().find('a')[0];
+        var songTitle = $(link[0]).parent().parent().find('a')[1];
+        var songAlbum = $(link[0]).parent().parent().find('a')[2];
+        nowPlaying = $(songArtist).text()+" - "+$(songTitle).text();
+        player.src = link.attr('href');
+        par = link.parent();
+        par.addClass('active-file').siblings().removeClass('active-file');
+        player.load();
+        player.play();
+      }
+      
+    }, {
+      tags: ["title","artist","album","year"],
+      dataReader: FileAPIReader(blob)
+    });
+  };
+  reader.readAsArrayBuffer(blob);
+
+}
---
+++ js/indexdb.js
@@ -0,0 +1,75 @@
+var db;
+ 
+function indexedDBOk() {
+    return "indexedDB" in window;
+}
+ 
+document.addEventListener("DOMContentLoaded", function() {
+ 
+    //No support? Go in the corner and pout.
+    if(!indexedDBOk) return;
+ 
+    var openRequest = indexedDB.open("songs",1);
+ 
+    openRequest.onupgradeneeded = function(e) {
+        var thisDB = e.target.result;
+ 
+        if(!thisDB.objectStoreNames.contains("songs")) {
+            thisDB.createObjectStore("songs",{keyPath:'filename'});
+        }
+    }
+ 
+    openRequest.onsuccess = function(e) {
+        console.log("running onsuccess");
+        
+        db = e.target.result;
+        setTimeout(addSongHistory(),5000);
+    }
+ 
+    openRequest.onerror = function(e) {
+        //Do something for the error
+    }
+ 
+},false);
+ 
+function  addSongBlobToIDB(url,filename,filetype,blob) {
+    console.log("About to add "+filename);
+ 
+    var transaction = db.transaction(["songs"],"readwrite");
+    var store = transaction.objectStore("songs");
+ 
+    //Define a person
+    var songBlob = {
+        filename:filename,
+        filetype:filetype,
+        blob:blob
+    }
+ 
+    //Perform the add
+    var request = store.add(songBlob);
+ 
+    request.onerror = function(e) {
+        console.log("Error",e.target.error.name);
+        //some type of error handler
+    }
+ 
+    request.onsuccess = function(e) {
+        linkToFile = URL.createObjectURL(blob)
+        console.log("e");
+        play_torrent_file(linkToFile, filename, filetype, blob);
+    }
+}
+
+function addSongHistory(){
+    var objectStore = db.transaction("songs").objectStore("songs");
+
+    objectStore.openCursor().onsuccess = function(event) {
+      var cursor = event.target.result;
+      if (cursor) {
+        play_torrent_file(URL.createObjectURL(cursor.value.blob),cursor.value.filename,cursor.value.filetype,cursor.value.blob);
+        cursor.continue();
+      }
+      else {
+      }
+    };
+}
