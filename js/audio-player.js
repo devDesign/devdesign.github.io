6077d72857218d7b270e97cd53337e8c5093fac3
devDesign <marlyguha@gmail.com>
2014-11-13 00:22:12 -0800

    bugs

--- js/audio-player.js
+++ js/audio-player.js
@@ -21,36 +21,53 @@ $('document').ready(function(){
 });
 
 function play_torrent_file(url, title, type, blob) {
-  blob['type']=type
   var audio;
   var playlist;
   var tracks;
   var current;
+  var first;
   reader = new FileReader();
   console.log(blob);
   
   reader.onload = function(event) {
     ID3.loadTags(url, function() {
-      var tags = ID3.getAllTags(url);
+
+      tags = ID3.getAllTags(url);
+      if(!tags.title){
+        tags.title=title;
+      }
+      if(!tags.year){
+        tags.year=" ";
+      }
+      if(!tags.track){
+        tags.track=" / ";
+      }
+      if(!tags.artist){
+        tags.artist=" ";
+      }
       var songRow = $('<tr class="file-entry"><td><a href='+url+'>'+tags.artist+'</a></td><td><a href='+url+'>'+tags.title+'</a></td><td><a href='+url+'>'+tags.album+'</a></td><td><a href='+url+'>'+tags.track.split("/")[0]+'</a></td><td><a href='+url+'>'+tags.year+'</a></td></tr>')
       songRow.appendTo('#playlist-tbody');
       $("#playlist").trigger('addRows',[songRow,true]); 
       initPlaylist(tags);
       function initPlaylist(tag){
+        first = 0
         current = 0;
         audio = $('#audioplayer')[0];
         playlist = $('#playlist-tbody');
         tracks = playlist.find('tr');
-        len = tracks.length - 2;
+        len = tracks.length - 1;
         audio.volume = .70;
-        audio.play();
        playlist.find('a').click(function(e){
+            first++;
+            $('.nowplaying').remove();
             e.preventDefault();
             link = $(this);
             current = link.parent().parent().index();
             run(link, audio);
         });
         audio.addEventListener('ended',function(e){
+            $('.nowplaying').remove();
+            first++;
             current++;
             if(current == len){
                 current = 0;
@@ -68,11 +85,14 @@ function play_torrent_file(url, title, type, blob) {
         var songTitle = $(link[0]).parent().parent().find('a')[1];
         var songAlbum = $(link[0]).parent().parent().find('a')[2];
         nowPlaying = $(songArtist).text()+" - "+$(songTitle).text();
+        $('<div />',{class:'nowplaying', text:nowPlaying}).appendTo('#now_playing');
         player.src = link.attr('href');
         par = link.parent();
         par.addClass('active-file').siblings().removeClass('active-file');
         player.load();
-        player.play();
+        if(first>1){
+          player.play();
+        }
       }
       
     }, {
